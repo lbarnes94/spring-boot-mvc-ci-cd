701e829e3cdcda3144dddbd44fd6b288246f3801
Luke Barnes <lukebarnes94@gmail.com>
2022-12-06 07:46:01 +0000

    attempt 2

--- src/main/java/com/ab/configs/SpringConfig.java
+++ src/main/java/com/ab/configs/SpringConfig.java
@@ -2,11 +2,10 @@ package com.ab.configs;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Configuration;
 
 import com.ab.SpringBootMvcCiCdApplication;
 
-@Configuration
+
 public class SpringConfig extends SpringBootServletInitializer{
 	
 	 @Override
