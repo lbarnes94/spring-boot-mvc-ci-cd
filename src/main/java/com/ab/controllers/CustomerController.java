430ecbd637d3697244741ed446b39ae44c38f880
Luke Barnes <lukebarnes94@gmail.com>
2022-12-06 07:40:10 +0000

    CustomerController added

---
+++ src/main/java/com/ab/controllers/CustomerController.java
@@ -0,0 +1,15 @@
+package com.ab.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerController {
+	
+	
+	@GetMapping("/")
+	public String home() {
+		return "Welcome to Spring MVC using CI / CD Pipeline";
+	}
+
+}
